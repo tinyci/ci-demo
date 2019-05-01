38df4167487525cdc577e06132d16c2874b43483
unclejack <unclejack@users.noreply.github.com>
2019-05-01 21:15:40 +0300

    clean up the tunnel setup code & add wget support

--- set_up_ngrok.sh
+++ set_up_ngrok.sh
@@ -3,22 +3,24 @@
 HAS_JQ=0
 HAS_PYTHON=0
 HAS_PYTHON3=0
+HAS_CURL=0
+HAS_WGET=0
 URL=
 TUNNEL_ENDPOINT_CONFIG=./tunnel-config.json
 TUNNEL_ENDPOINT_RESULT=tunnel.json
 PYTHON_JSON_DUMP_CODE="import sys, json; print(json.load(sys.stdin)['public_url'])"
 
-jq
+jq >/dev/null 2>&1
 if [ "$?" -eq "0" ]; then
 	HAS_JQ=1
 fi
 
-python -V
+python -V >/dev/null 2>&1
 if [ "$?" -eq "0" ]; then
 	HAS_PYTHON=1
 fi
 
-python3 -V
+python3 -V >/dev/null 2>&1
 if [ "$?" -eq "0" ]; then
 	HAS_PYTHON3=1
 fi
@@ -28,9 +30,34 @@ if [ "$HAS_JQ" -eq "0" ] && [ "$HAS_PYTHON" -eq "0" ] && [ "$HAS_PYTHON3" -eq "0
 	exit 1
 fi
 
+curl -V >/dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+	HAS_CURL=1
+fi
+
+wget -V >/dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+	HAS_WGET=1
+fi
+
+if [ "$HAS_CURL" -eq "0" ] && [ "$HAS_WGET" -eq "0" ]; then
+	echo "curl or wget must be installed"
+	exit 1
+fi
+
 set -euo pipefail
 
-wget -O $TUNNEL_ENDPOINT_RESULT --post-file=$TUNNEL_ENDPOINT_CONFIG --header='Content-Type:application/json' http://127.0.0.1:4040/api/tunnels
+if [ "$HAS_CURL" -eq "1" ]; then
+	curl -o $TUNNEL_ENDPOINT_RESULT \
+		-H "Content-Type: application/json" \
+		-d @$TUNNEL_ENDPOINT_CONFIG \
+		http://127.0.0.1:4040/api/tunnels
+elif [ "$HAS_WGET" -eq "1" ]; then
+	wget -O $TUNNEL_ENDPOINT_RESULT \
+		--post-file=$TUNNEL_ENDPOINT_CONFIG \
+		--header='Content-Type:application/json' \
+		http://127.0.0.1:4040/api/tunnels
+fi
 
 if [ "$HAS_JQ" -eq "1" ]; then
 	URL=$(cat $TUNNEL_ENDPOINT_RESULT | jq -r '.public_url')
